f164597a43588e4c803a2b91c87201afdc34329a
mconti <posta@maurizioconti.com>
2021-10-22 17:57:28 +0200

    Prima versione

---
+++ Models/Assenza.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace maurizio.conti._5H.DocentiAssenti
+{
+    public class Assenza {
+
+        public DateTime Ora {get;set;} 
+        public String Classe {get;set;} 
+        public String DocenteAssente {get;set;} 
+        public String Sostituto1 {get;set;} 
+        public String Sostituto2 {get;set;} 
+        public string Pagamento {get;set;} 
+        public String Note {get;set;} 
+        public String Firma {get;set;} 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maurizio.conti._5H.DocentiAssenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
